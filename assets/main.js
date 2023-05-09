b929e56f300ea029793644978c68f57742716660
julian.guarin.arus <julian6732@gmail.com>
2023-05-09 08:19:00 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -1,12 +1,10 @@
-
-
-const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLNSaVuGJb3muKM6YQwlbGOM-Uvc4fGuTd&part=snippet&maxResults=10';
 const content=null || document.getElementById('content');
+const API = 'https://linkedin-profile-data.p.rapidapi.com/linkedin-data?url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fjulian-guarin-m';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'be8488ecb7msh41268d4149dcadap1210a1jsn1231b6add235',
-		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+		'X-RapidAPI-Host': 'linkedin-profile-data.p.rapidapi.com'
 	}
 };
 
@@ -17,20 +15,25 @@ async function fetchData(urlApi){
   return data;
 }
 
+
 (async ()=>{
   try{
-    const videos=await fetchData (API);
+    const cursos=await fetchData (API);
     let view = `
-    ${videos.items.map(video => `
+    ${cursos.education.map(curso => `
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${curso.logo_url}" alt="Logo ${curso.school.name}" class="w-full">
         </div>
-        <div class="mt-4 flex justify-between">
+        <div class="mt-4 flex-row justify-between">
           <h3 class="text-sm text-gray-700">
             <span aria-hidden="true" class="absolute inset-0"></span>
-            ${video.snippet.title}
+            ${curso.degree_name}
+          </h3>
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${curso.school}
           </h3>
         </div>
       </div>
